730f8506b1d0a3dbacf1c3e59572cbf761e9e5d9
Paradoxial <teitelmanevan@gmail.com>
2012-02-03 21:36:40 -0500

    Conversion complete. Site is now ready for additional pages

---
+++ Website/components.php
@@ -0,0 +1,136 @@
+<?php
+//Loads head (EG analytics, title, document configuration, etc)
+function loadHead($title)
+{
+?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title><?php echo "PygmyOS | " . $title ?></title>
+    <link rel="Stylesheet" type="text/css" href="styles/style.css" />
+<?php
+}
+
+//Displays the page header
+function displayHeader($title = "PygmyOS", $icon = "images/Icon2BlueGlow.png")
+{
+?>
+    <div id="header">
+        <a href="index.html">
+            <img src=<?php echo $icon ?> alt="Logo" width="100px" height="100px" id="logo" />
+        </a>
+        <h1 id="head_title">
+        <?php echo $title ?> </h1>
+    </div>
+<?php
+}
+
+//Displays the page navigation bar
+function displayNavbar( $links = array('index.html', 'features.html', '', '', 'contact.html'), 
+                        $linkNames = array('Home', 'Code', 'Boards', 'Projects', 'Contact'), 
+                        $linkPageIDs = array('nav-home', 'nav-code', 'nav-boards', 'nav-projects', 'nav-contact'))
+{
+?>
+    <div id="navbar">
+        <div id="site-bar">
+            <ul>
+                <?php
+                if(is_array($links) && is_array($linkNames) && is_array($linkPageIDs))
+                {
+                    if(count($links) == count($linkNames) && count($links) == count($linkPageIDs))
+                    {
+                        for($i=0;$i<count($links);$i++)
+                        {
+                            if($i==0)
+                            {
+                                echo '<li class="firstlink">';
+                            }
+                            else if($i==count($links)-1)
+                            {
+                                echo '<li class="lastlink">';
+                            }
+                            else
+                            {
+                                echo '<li>';
+                            }
+                            
+                            echo '<a href="' . $links[$i] . '" id="' . $linkPageIDs[$i] . '">' . $linkNames[$i] . '</a></li>';
+                        }
+                        /*<li class="firstlink"><a href="index.html" id="nav-home">Home</a></li>
+                        <li><a href="features.html" id="nav-code">▼ Code</a></li>
+                        <li><a href="" id="nav-boards">Boards</a></li>
+                        <li><a href="" id="nav-projects">Projects</a></li>
+                        <li class="lastlink"><a href="contact.html" id="nav-contact">Contact</a></li>*/
+                    }
+                    else
+                    {
+                        throw new Exception('Links, link names, and link page IDs must be passed in equal numbers');
+                    }
+                }
+                else
+                {
+                    throw new Exception('Links must be passed to navbar as arrays.');
+                }
+                ?>
+            </ul>
+        </div>
+        <div id="social-bar">
+            <ul>
+                <!--<li class="youtube"><a href="http://www.youtube.com/sparkfun" target="_blank" class="hidetext">
+                    <span>YouTube</span></a></li>-->
+                <!--<li class="vimeo"><a href="http://www.vimeo.com/sparkfun" target="_blank" class="hidetext">
+                    <span>Vimeo</span></a></li>-->
+                <li class="gplus"><a href="https://plus.google.com/106638354592939322299" class="hidetext" target="_blank">
+                    <span>Google Plus</span></a></li>
+                <li class="facebook"><a href="http://www.facebook.com/PygmyOS" class="hidetext" target="_blank"><span>
+                    Facebook</span></a></li>
+                <li class="flickr"><a href="http://www.flickr.com/photos/58435130@N08/sets/72157627768508212/"
+                    target="_blank" class="hidetext" target="_blank"><span>Flickr</span></a></li>
+                <li class="twitter"><a href="https://twitter.com/#!/PygmyOS" class="hidetext" target="_blank"><span>
+                    Twitter</span></a></li>
+                <li class="rss"><a href="http://pygmyos.wordpress.com/feed/" class="hidetext" target="_blank"><span>
+                    RSS</span></a></li>
+                <!--<li class="icon"><a href="index.html"></a></li>-->
+            </ul>
+        </div>
+    </div>
+<?php
+}
+
+//Displays the picture carousel
+function displayCarousel($pictures)
+{
+    if(is_array($pictures))
+    {
+        ?>
+        <div id="carousel">
+            <div class="slides">
+                <div class="home_text" style="position: absolute; top: 0px; left: 0px; visibility: visible;
+                    zoom: 1; z-index: 1; opacity: 1;">
+                    <?php
+                        foreach($pictures as $picture)
+                        {
+                            echo '<img src="' . $picture . '" alt="Pygmy pictures" />';
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    else
+    {
+        throw new Exception('Pictures must be passed to the carousel as an array.');
+    }
+}
+
+//Displays the footer
+function displayFooter()
+{
+?>
+    <div id="footer">
+        PygmyOS Website Design and Copyright &copy; 2012 <a href="http://www.sparkfun.com/users/154228">
+            Evan Teitelman</a> | <a href="http://code.google.com/p/pygmyos/source/browse/?repo=site#git%2FWebsite" target="_blank">Site source</a><br />
+        PygmyOS Logo and Trademark Copyright &copy; 2012 Warren D Greenway
+    </div>
+<?php
+}
+?>
--- Website/index.php
+++ Website/index.php
@@ -1,16 +1,17 @@
-<?php require("components.php"); ?>
+<?php   $page_id = "home";
+        require("components.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <?php loadHead("Home"); ?>
 </head>
-<body id="home">
+<body id=<?php echo $page_id ?>>
     <div id="container">
         <?php   displayHeader();
-                displayNavbar(); 
-                displayCarousel(); ?>
-        <div id="content" style="display:inline-block; background-color: #FFFFFF; padding: 20px 20px 20px 20px;">
+                displayNavbar();
+                displayCarousel(array("images/boards/Nebula1.jpg")); ?>
+        <div id="content">
             <h4>
                 Current status</h4>
             <p>
@@ -29,57 +30,56 @@
             </p>
             <br />
             <h4>
-                Features</h4>
-            <ul>
-                <li>Multitasking</li>
-                <li>Inter-process messaging</li>
-                <li>Command interface with queuing</li>
-                <li>Real Time Clock with calender</li>
-                <li>Stopwatch for process timing</li>
-                <li>Nested Vector Interrupt Controller interface simplifies interrupt driven application
-                    development</li>
-                <li>Substantial software and hardware USART, I2C, SPI, and parallel support Extensible
-                    FIFO and stream support</li>
-                <li>Formatted output</li>
-                <li>Advanced Pygmy Data Integrity Algorithm integrated with formatted output</li>
-                <li>Simple enough for a beginner to use</li>
-            </ul>
+            Features</h4>
+            <p>
+                <ul>
+                    <li>Multitasking</li>
+                    <li>Inter-process messaging</li>
+                    <li>Command interface with queuing</li>
+                    <li>Real Time Clock with calender</li>
+                    <li>Stopwatch for process timing</li>
+                    <li>Nested Vector Interrupt Controller interface simplifies interrupt driven application
+                        development</li>
+                    <li>Substantial software and hardware USART, I2C, SPI, and parallel support Extensible
+                        FIFO and stream support</li>
+                    <li>Formatted output</li>
+                    <li>Advanced Pygmy Data Integrity Algorithm integrated with formatted output</li>
+                    <li>Simple enough for a beginner to use</li>
+                </ul>
+            </p>
+            <br />
             <h4>
-                Nebula Features</h4>
-            <ul>
-                <li>2” round board outline</li>
-                <li>STM32F100 24MHz, STM32F103 72MHz</li>
-                <li>Cutting edge ARM Cortex M3</li>
-                <li>Support for PygmyOS</li>
-                <li>Integrated bootloader</li>
-                <li>4MB Flash with filesystem</li>
-                <li>2.4GHz RF</li>
-                <li>LiPo battery charge and management</li>
-                <li>On-board power regulation</li>
-                <li>Precision temperature sensor</li>
-                <li>Header with 25 general purpose IO pins
-                    <ul>
-                        <li>Master clock out, timer pin</li>
-                        <li>2 USART TX/RX pairs, digital pins</li>
-                        <li>4 Timer pins</li>
-                        <li>2 Time/Analog pins</li>
-                        <li>8 Analog pins</li>
-                        <li>2 DAC ( Digital to Analog Converter ) pins</li>
-                        <li>3.3V Power</li>
-                        <li>5V Input</li>
-                        <li>Battery</li>
-                        <li>GND</li>
-                    </ul>
-                </li>
-            </ul>
+            Nebula Features</h4>
+            <p>
+                <ul>
+                    <li>2” round board outline</li>
+                    <li>Straight headers with 25 general purpose IO pins
+                        <ul class="nomargin"">
+                            <li>Master clock out, timer pin</li>
+                            <li>2 USART TX/RX pairs, digital pins</li>
+                            <li>4 Timer pins</li>
+                            <li>2 Time/Analog pins</li>
+                            <li>8 Analog pins</li>
+                            <li>2 DAC ( Digital to Analog Converter ) pins</li>
+                            <li>3.3V Power</li>
+                            <li>5V Input</li>
+                            <li>Battery</li>
+                            <li>GND</li>
+                        </ul>
+                    </li>
+                    <li>STM32F100 24MHz, STM32F103 72MHz</li>
+                    <li>Cutting edge ARM Cortex M3</li>
+                    <li>2.4GHz RF</li>
+                    <li>LiPo battery charge and management</li>
+                    <li>On-board power regulation</li>
+                    <li>Support for PygmyOS</li>
+                    <li>Integrated bootloader</li>
+                    <li>4MB Flash with filesystem</li>
+                    <li>Precision temperature sensor</li>
+                </ul>
+            </p>
         </div>
-        <div id="footer" style="background-color: #f2f2ee; height: auto; border-top: 1px solid #3072b3;
-            color: #93948c;">
-            <p id="copyright" style="margin: 0; padding: 3px 3px 3px 3px; text-align: center;">
-                PygmyOS Website Design and Copyright &copy; 2012 <a href="http://www.sparkfun.com/users/154228">
-                    Evan Teitelman</a><br />
-                PygmyOS Logo and Trademark Copyright &copy; 2012 Warren D Greenway</p>
-        </div>
+        <?php displayFooter(); ?>
     </div>
 </body>
 </html>
