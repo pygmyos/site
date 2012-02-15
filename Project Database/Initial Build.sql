d9c40787c80ebda7eb057fa7c849c1c15e47121c
Paradoxial <teitelmanevan@gmail.com>
2012-02-15 16:04:53 -0500

    Continued work on the database build code
    The ERD no longer matches the build code due to small changes made while writing the code. It will have to catch up

--- Project Database/Initial Build.sql
+++ Project Database/Initial Build.sql
@@ -46,7 +46,7 @@ PRIMARY KEY (Id)
 CREATE TABLE incoming_orders
 (
 Id mediumint UNSIGNED NOT NULL AUTO_INCREMENT,
-Status enum('exception', 'packed', 'label printed', 'ready to ship', 'shipped', 'received', 'cancelled') NOT NULL,
+Status enum('ship error', 'new order', 'shipped', 'received', 'cancelled') NOT NULL,
 Tracking_Nbr tinytext,
 Date_Created datetime NO NULL,
 Delivery_Estimate date NO NULL,
@@ -68,7 +68,7 @@ PRIMARY KEY (Order_Id, Part_Id)
 CREATE TABLE outgoing_orders
 (
 Id mediumint UNSIGNED NOT NULL AUTO_INCREMENT,
-Status enum('exception', 'packed', 'label printed', 'ready to ship', 'shipped', 'cancelled') NOT NULL,
+Status enum('exception', 'new order', 'packed', 'label printed', 'ready to ship', 'shipped', 'cancelled') NOT NULL,
 Tracking_Nbr tinytext,
 Date_Created datetime NO NULL,
 Delivery_Estimate date NO NULL,
@@ -115,6 +115,28 @@ Weight smallint,
 PRIMARY KEY (Id)
 );
 
+-- Part subtypes
+CREATE TABLE components
+(
+);
+
+CREATE TABLE boards
+(
+);
+
+CREATE TABLE pcbs
+(
+);
+
+CREATE TABLE stencils
+(
+);
+
+CREATE TABLE other_parts
+(
+);
+
+-- Part related
 CREATE TABLE part_prices
 (
 Start_Date date NOT NULL,
@@ -139,7 +161,7 @@ CREATE TABLE documents
 (
 Id smallint UNSIGNED NOT NULL AUTO_INCREMENT,
 Url tinytext NOT NULL,
-Description mediumtext NOT NULL,
+Description text NOT NULL,
 PRIMARY KEY (Id)
 );
 
@@ -155,7 +177,9 @@ PRIMARY KEY (Part_Id, Number)
 
 CREATE TABLE libraries
 (
-
+LibraryName tinytext NOT NULL,
+Description text,
+PRIMARY KEY (LibraryName)
 );
 
 CREATE TABLE part_ownerships
@@ -184,11 +208,7 @@ CREATE TABLE vendors
 (
 );
 
--- Boards
-CREATE TABLE boards
-(
-);
-
+-- Board (subtype) related
 CREATE TABLE part_usages
 (
 );
@@ -197,47 +217,106 @@ CREATE TABLE pins
 (
 );
 
--- Components
+-- Component (subtype) related
 CREATE TABLE variants
 (
+Part_Id smallint NOT NULL,
+Package_Id smallint NOT NULL,
+FOREIGN KEY (Part_Id) REFERENCES Parts(Id),
+FOREIGN KEY (Package_Id) REFERENCES Packages(Id),
+PRIMARY KEY (Part_Id, Package_Id)
 );
 
 CREATE TABLE packages
 (
+Package_Name tinytext NOT NULL,
+Mount_Type enum('surface mount', 'through hole', 'connector', 'other'),
+Number_Pins tinyint,
+Description text,
+Url tinytext,
+PRIMARY KEY (Package_Name)
 );
 
 -- Pictures
 CREATE TABLE pictures
 (
+Id smallint UNSIGNED NOT NULL AUTO_INCREMENT,
+Url tinytext NOT NULL,
+Caption tinytext NOT NULL,
+Flickr_Url tinytext,
+Page_Id tinytext,
+FOREIGN KEY (Page_Id) REFERENCES Pages(Id)
+PRIMARY KEY (Id)
 );
 
 CREATE TABLE picture_tags
 (
+Number tinyint UNSIGNED NOT NULL AUTO_INCREMENT,
+Left_Corner_X smallint UNSIGNED,
+Left_Corner_Y smallint UNSIGNED,
+Right_Corner_X smallint UNSIGNED,
+Right_Corner_Y smallint UNSIGNED,
+Alternative_Text tinytext,
+Part_Id smallint,
+Picture_Id smallint NOT NULL,
+FOREIGN KEY (Picture_Id) REFERENCES Pictures(Id)
+FOREIGN KEY (Part_Id) REFERENCES Parts(Id)
+PRIMARY KEY (Picture_Id, Number)
 );
 
 -- Pages
 CREATE TABLE pages
 (
+Id tinytext NOT NULL,
+Name tinytext NOT NULL,
+Title tinytext NOT NULL,
+Has_Header bit NOT NULL DEFAULT 1,
+Has_Navbar bit NOT NULL DEFAULT 1,
+Has_Socbar bit NOT NULL DEFAULT 1,
+Has_Footer bit NOT NULL DEFAULT 1,
+Content mediumtext NOT NULL DEFAULT 'No content',
+Archived bit NOT NULL DEFAULT 0,
+Custom_Url tinytext,
+Page_Type enum('other', 'part', 'board', 'tutorial'),
+Part_Id smallint,
+FOREIGN KEY (Part_Id) REFERENCES Parts(Id),
+PRIMARY KEY (Id)
 );
 
 CREATE TABLE page_relations
 (
+Visible bit NOT NULL DEFAULT 1,
+Page1_Id tinytext NOT NULL,
+Page1_Id tinytext NOT NULL,
+FOREIGN KEY (Page1_Id) REFERENCES Pages(Id),
+FOREIGN KEY (Page2_Id) REFERENCES Pages(Id),
+PRIMARY KEY (Page1_Id, Page2_Id)
 );
 
-CREATE TABLE alt_nav_links
+CREATE TABLE alternative_nav_links
 (
+Text tinytext,
+Page1_Id tinytext NOT NULL,
+Page1_Id tinytext NOT NULL,
+FOREIGN KEY (Page1_Id) REFERENCES Pages(Id),
+FOREIGN KEY (Page2_Id) REFERENCES Pages(Id),
+PRIMARY KEY (Page1_Id, Page2_Id)
 );
 
 CREATE TABLE comments
 (
+Id smallint UNSIGNED NOT NULL AUTO_INCREMENT,
+Page_Id smallint NOT NULL,
+FOREIGN KEY (Page_Id) REFERENCES Pages(Id)
+PRIMARY ID (Page_Id, Id)
 );
 
 CREATE TABLE comment_texts
 (
-);
-
-CREATE TABLE 
-(
+Edit_Number UNSIGNED NOT NULL AUTO_INCREMENT,
+Comment_Id smallint NOT NULL,
+FOREIGN KEY (Comment_Id) REFERENCES Comments(Id),
+PRIMARY KEY (Comment_Id, Edit_Number)
 );
 
 
